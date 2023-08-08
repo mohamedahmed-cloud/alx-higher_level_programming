ea3a27bdf0b8926e3b4233f0c5579ba1a6a8be5b
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-09 00:12:24 +0300

    Add File 11-pow.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz():
+    for i in range(1, 101):
+        ans = ""
+        if i % 3 == 0:
+            ans +="Fizz"
+        if i % 5 == 0:
+            ans += "Buzz"
+        if not ans:
+            print(i, end=' ')
+            continue
+        print("{:s} ".format(ans))
+        
