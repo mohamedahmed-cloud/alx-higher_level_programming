932ee0a7ba97d2a90f3363783f106e711f22e813
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-08 21:31:15 +0300

    Add File 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,11 @@ last_digit = abs(number) % 10
 mul = 1
 if number < 0:
     mul *= -1
-if last_digit > 5:
+if last_digit * mul > 5:
     print(f"Last digit of {number :d} is {last_digit * mul:d} and\
  is greater than 5")
-elif last_digit == 0:
+elif last_digit * mul == 0:
     print(f"Last digit of {number :d} is {last_digit * mul:d} and is 0")
-elif last_digit < 6:
+elif last_digit * mul < 6:
     print(f"Last digit of {number :d} is {last_digit * mul:d} and is less\
  than 6 and not 0")
