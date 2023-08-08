aae75cfb8495611b9b1b0b92d3802cdb047019ab
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-08 21:49:33 +0300

    Add File 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if i == ord('q') or i == ord('e'):continue
+    if i == ord('q') or i == ord('e'):
+        continue
     print("{:s}".format(chr(i)), end="")
