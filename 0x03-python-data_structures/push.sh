21b8d58f3ae2743ce6352b7842d9818e895bcf00
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-17 21:03:31 +0300

    add File 0-print_list_integer.py

--- 0x03-python-data_structures/push.sh
+++ 0x03-python-data_structures/push.sh
@@ -6,3 +6,14 @@ if [ $? -ne 1 ];then
 	git commit -m "add File $1"
 	git push
 fi
+
+# 
+# pycodestyle $1
+# if [ $? -ne 1 ]; then
+# 	echo "Code Style End"
+# 	git add --chmod +x $1
+# 	git add * 
+# 	git commit -m "Add File $1"
+# 	git push
+	
+# fi
