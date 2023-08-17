96a3b3aa04dd1d912ec0f44ee6e3e06e1b694e34
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-17 12:12:36 +0300

    Add File 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
-from sys import argv
-n = len(argv)
-if n == 1:
-    print("0 arguments.")
-elif n == 2:
-    print("1 argument:")
-    print("1: {:}".format(argv[1]))
-else:
-    print("{:} arguments:".format(n))
-    for i in range(1, n):
-        print("{:}: {:}".format(i, argv[i]))
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    n = len(argv)
+    if n == 1:
+        print("0 arguments.")
+    elif n == 2:
+        print("1 argument:")
+        print("1: {:}".format(argv[1]))
+    else:
+        print("{:} arguments:".format(n))
+        for i in range(1, n):
+            print("{:}: {:}".format(i, argv[i]))
