57a449fc4c3fe9092d76ecc87d3b8511c2788d16
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-17 12:13:53 +0300

    Add File 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
         print("1 argument:")
         print("1: {:}".format(argv[1]))
     else:
-        print("{:} arguments:".format(n))
+        print("{:} arguments:".format(n - 1))
         for i in range(1, n):
             print("{:}: {:}".format(i, argv[i]))
