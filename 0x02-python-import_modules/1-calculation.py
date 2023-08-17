1ba2b5fec2a9168ab8ee02018aa038fcd058e25d
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-17 11:40:02 +0300

    Add File 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
         import calc function then use it to make clac on them	.
     '''
     from calculator_1 import add, sub, mul, div
-    a = 1
-    b = 2
+    a = 10
+    b = 5
     print("{:} + {:} = {:}".format(a, b, add(a, b)))
     print("{:} + {:} = {:}".format(a, b, sub(a, b)))
     print("{:} + {:} = {:}".format(a, b, mul(a, b)))
