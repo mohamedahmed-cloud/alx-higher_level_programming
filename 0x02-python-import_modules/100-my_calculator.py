9a5b564622cc43475aa653628dba323f592efc67
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-17 15:25:25 +0300

    Add File 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,33 +6,33 @@ if __name__ == "__main__":
     allowed = "+-*/"
     n -= 1
     print(argv)
-    
+
     if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-        
+
     a = int(argv[1])
     operator = argv[2]
     print(operator)
-    
+
     b = int(argv[3])
     if operator not in allowed:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    
+
     match operator:
         case "+":
             from calculator_1 import add
             print("{:} + {:} = {:}".format(a, b, add(a, b)))
-            
+
         case "-":
             from calculator_1 import sub
             print("{:} - {:} = {:}".format(a, b, sub(a, b)))
-            
+
         case "*":
             from calculator_1 import mul
             print("{:} * {:} = {:}".format(a, b, mul(a, b)))
-            
+
         case "/":
             from calculator_1 import div
             print("{:} / {:} = {:}".format(a, b, div(a, b)))
