6901860546cb41069c258fb713d7395e1f07197a
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-22 18:13:02 +0300

    Add File 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    shallowCopy = a_dictionary.copy()
+    deleted_key = []
     for key, val in a_dictionary.items():
         if val == value:
-            del shallowCopy[key]
-    return shallowCopy
+            deleted_key.append(key)
+    for x in deleted_key:
+        del a_dictionary[x]
+    return a_dictionary
