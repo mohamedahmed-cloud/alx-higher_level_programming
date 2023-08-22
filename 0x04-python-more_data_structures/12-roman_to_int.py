15b4fc2b837d5229c224db941a4b797f47acb5a0
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-22 17:45:38 +0300

    Add File 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    for key, value in a_dictionary.items():
-        a_dictionary[key] *= 2
+def roman_to_int(roman_string):
+
+    all = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
+           "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+           "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
+           "", "M", "MM", "MMM"]
+
+    def find_order(curr):
+        if "I" in curr or "V" in curr:
+            return 1
+        elif "X" in curr or "L" in curr:
+            return 10
+        elif "C" in curr or "D" in curr:
+            return 100
+        elif "M" in curr:
+            return 1000
+
+    curr = ""
+    ans = 0
+
+    for i in roman_string:
+        curr += i
+        if curr not in all:
+            ans += (all.index(curr[:-1]) % 10) * find_order(curr[:-1])
+            curr = curr[-1]
+
+    ans += (all.index(curr) % 10) * find_order(curr)
+    return ans
