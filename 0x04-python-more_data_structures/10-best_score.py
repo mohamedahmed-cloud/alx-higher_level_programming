05c14a9a63b3fc3698b3a21b3b9452f8cd2e92e8
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-08-22 16:28:04 +0300

    Add File 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    ans = ""
+    m = -float("inf")
+    if not a_dictionary:
+        return None
+    for key, value in a_dictionary.items():
+        if value > m:
+            m = value
+            ans = key
+    return ans
