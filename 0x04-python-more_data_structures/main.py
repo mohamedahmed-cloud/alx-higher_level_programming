142731247a19e026dfb9764f0f0b4141d10dba9d
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-01 17:49:52 +0300

    Change Main file

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-complex_delete = __import__('102-complex_delete').complex_delete
-print_sorted_dictionary = \
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+def f():
+    excs = [OSError('error 1'), SystemError('error 2')]
+    raise ExceptionGroup('there were problems', excs)
+f()
 
-my_dict = { 'a': "a", 'b': "b" , 'c': "c", 'd': "d", 'e': "e" }
-value = 'a'
-new_dict = complex_delete(my_dict, value)
-print_sorted_dictionary(my_dict)
-print("xx")
-print_sorted_dictionary(new_dict)
