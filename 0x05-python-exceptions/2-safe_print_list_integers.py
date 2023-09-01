08f5355acb6b684140572fd6c42ee31f457450fc
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-01 12:00:07 +0300

    Add File 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+
+    cnt = 0
+    for i in my_list:
+        cnt += 1
+
+    try:
+        if x <= cnt:
+            value = int("".join(str(x) for x in my_list if isinstance(x, int)))
+            cnt = sum(1 for x in my_list if isinstance(x, int))
+            print("{:d}".format(value))
+            return min(x, cnt)
+        else:
+            raise IndexError("list index out of range")
+
+    except IndexError:
+        raise
