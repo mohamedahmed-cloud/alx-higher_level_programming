e73088492b6e2e600b2b3f13e9d48eb6562f47fe
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-01 23:11:35 +0300

    Add File 3-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+""" Module contain a definition for Square Class"""
+
+
+class Square:
+    """
+        This is the square class.
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
