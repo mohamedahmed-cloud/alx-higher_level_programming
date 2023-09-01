0aa337569908307af3a0cbc649e0b8e0cd561d3f
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-01 14:45:49 +0300

    Add File 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    n1 = len(my_list_1)
+    n2 = len(my_list_2)
+    result = [0] * max(n1, n2)
+
+    for i in range(max(n1, n2)):
+        try:
+            result[i] = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            result[i] = 0
+            print("out of range")
+        except ZeroDivisionError:
+            result[i] = 0
+            print("division by 0")
+        except TypeError:
+            result[i] = 0
+            print("wrong type")
+    return result
