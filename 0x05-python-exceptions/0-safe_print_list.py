54fc13b6457a14969d67a1fc9ebb25060cde2169
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-01 11:27:56 +0300

    Add File 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    cnt = 0
+    for i in my_list:
+        cnt += 1
     try:
         print("".join(map(str, my_list[:x])))
-        return min(x, len(my_list))
+        return min(x, cnt)
     except ValueError:
-        return min(x, len(my_list))
+        return min(x, cnt)
