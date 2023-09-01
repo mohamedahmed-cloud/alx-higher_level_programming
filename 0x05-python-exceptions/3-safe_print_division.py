ecb9912d54ed433c26757a1743ff7932f6e785d5
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-01 12:55:12 +0300

    Add File 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+        return result
+    except (TypeError, ZeroDivisionError):
+        return None
+    finally:
+        print("Inside result: {}".format(result))
--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-safe_print_list_integers = \
-    __import__('2-safe_print_list_integers').safe_print_list_integers
+safe_print_division = __import__('3-safe_print_division').safe_print_division
 
-my_list = [1, 2, 3, 4]
-x = 0
-nb_print = safe_print_list_integers(my_list, x)
-print("{:d}".format(nb_print))
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
