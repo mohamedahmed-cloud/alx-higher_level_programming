ec19d141e2afc7bdac414a2a6ed863954e05aabc
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-02 11:20:03 +0300

    Add File 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import math
-
-
 """
 Magic Class Implementation.
 """
+
+import math
 
 
 class MagicClass:
--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-Square = __import__('103-magic_class').Square
+Square = __import__('103-magic_class')
 
 print(Square.__doc__)
+print(Square.MagicClass.__doc__)
+# print(Square.MyClass)
