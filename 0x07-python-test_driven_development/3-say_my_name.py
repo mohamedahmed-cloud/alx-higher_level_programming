92b0adc32192e4691ead053fbdf78b3c77e0f978
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-04 12:49:48 +0300

    Add File 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -19,7 +19,4 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    if last_name:
-        print(f"My name is {first_name} {last_name}")
-    else:
-        print(f"My name is {first_name}")
+    print(f"My name is {first_name} {last_name}")
