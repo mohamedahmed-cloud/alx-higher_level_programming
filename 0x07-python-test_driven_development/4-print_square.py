4d5d6b175e66e922a1d3eaec451222a40773343e
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-04 13:12:09 +0300

    Add File 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+print_square - print square by #
+"""
+
+
+def print_square(size):
+    """
+        print_square: print square by type #
+        args:
+            size: the length of the square
+        return:
+            return the square with area size * size consist of #
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print((("#" * size + "\n") * size)[:-1])
