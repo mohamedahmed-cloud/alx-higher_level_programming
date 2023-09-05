d797946ab23a1bc1910eec6c67f32c7c71f5f7c5
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-05 14:30:20 +0300

    Add File 2-rectangle.py

--- 0x08-python-more_classes/UnitTest.py
+++ 0x08-python-more_classes/UnitTest.py
@@ -13,14 +13,22 @@ class RectangleTest(unittest.TestCase):
         r = Rectangle()
         self.assertIsInstance(r, Rectangle)
 
-    # 1-rectangle.py
+    # 1-rectangle.py test
     def test_CheckValue(self):
         r = Rectangle(1,3)
         self.assertAlmostEquals(r.width, 1)
         self.assertAlmostEquals(r.height, 3)
         self.assertRaises(ValueError, lambda: Rectangle(-2, 1))
         self.assertRaises(ValueError, lambda: Rectangle(2, -1))
-        self.assertRaises(ValueError, lambda: Rectangle(2, "Yousef"))
-        self.assertRaises(ValueError, lambda: Rectangle("Yousef", 1))
-    
+        self.assertRaises(TypeError, lambda: Rectangle(2, "Yousef"))
+        self.assertRaises(TypeError, lambda: Rectangle("Yousef", 1))
+
+    # 2-rectangle.py test 
+    def test_area_perimeter(self):
+        r = Rectangle(3,4)
+        self.assertAlmostEqual(r.area(), 12)
+        self.assertAlmostEqual(r.perimeter(), 14)
+        r = Rectangle(0,0)
+        self.assertAlmostEqual(r.area(), 0)
+        self.assertAlmostEqual(r.perimeter(), 0)
 
