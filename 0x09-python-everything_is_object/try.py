2b810ca8f47836e951f693e667cae9ee64d9411d
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-15 18:02:56 +0300

    Update File 105-line1.txt

--- 0x09-python-everything_is_object/try.py
+++ 0x09-python-everything_is_object/try.py
@@ -1,15 +1,2 @@
 #!/usr/bin/python3
-
-class Point2D:
-    __slots__ = ('x', 'y')
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __repr__(self):
-        return f'Point2D({self.x},{self.y})'
-
-obj = Point2D(2, 3)
-print(obj.__slots__)
-print(Point2D.__dict__)
+print(6 + 256)
