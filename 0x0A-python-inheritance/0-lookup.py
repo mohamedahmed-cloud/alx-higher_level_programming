002a7e48d87afc19ea70bbeb2faff8316581dac1
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-16 13:07:07 +0300

    UpLoad File

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-"lookup: return all info about any object"
+"""
+lookup: return all info about any object
+"""
 def lookup(obj):
+    """
+    args:
+        obj: the object to get info about it.
+    """
     return dir(obj)
--- 0x0A-python-inheritance/doc.py
+++ 0x0A-python-inheritance/doc.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
 module = __import__(argv[1][:-3])
+function = getattr(module, argv[1][2:-3], None)
 
 print(module.__doc__)
+print(function.__doc__)
+
