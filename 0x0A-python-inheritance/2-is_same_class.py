2384e9feef2f490d4e85ed561d34cdd9e81bfe45
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-16 14:30:30 +0300

    UpLoad File 1-my_list.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+is_same_class: class check the type of of a class
+"""
+def is_same_class(obj, a_class):
+    """
+    args:
+    obj: object to check it's type
+    a_class: class to compare with obj arguments
+    """
+    return isinstance(obj, a_class)
