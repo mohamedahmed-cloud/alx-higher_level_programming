b06fe87583aa6fff7d3cdc8f515226e7e804619b
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-16 16:19:01 +0300

    UpLoad File 7-base_geometry.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+BaseGeometry: BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+        BaseGeometry class - area class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name=-float("inf"), value=-float("inf")):
+        if value == -float("inf") or name == -float("inf"):
+            return
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
