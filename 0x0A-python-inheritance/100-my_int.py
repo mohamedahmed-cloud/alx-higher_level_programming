07502eed645350ca8e27f547c7ddab849ed6b91a
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-16 18:29:40 +0300

    UpLoad File 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,9 +3,13 @@
 MyInt - int class that inherit from int python class
 """
 
+
 class MyInt(int):
+    """
+    MyInt - int class that inherit from int python class
+    """
     def __eq__(self, other):
-           return self != other
+        return super().__ne__(other)
 
     def __ne__(self, other):
-        return self == other
+        return super().__eq__(other)
