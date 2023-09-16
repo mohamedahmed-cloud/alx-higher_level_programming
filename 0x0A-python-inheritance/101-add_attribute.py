cca4f7c2bd1812558e0cd9102bce4a617c28e854
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-16 18:38:14 +0300

    UpLoad File 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+add_attribute - implementation the function that can
+    add attribute to curr object
+"""
+
 
 def add_attribute(obj, attribute, value):
+    """
+    args:
+     1. obj - curr object to add attributes
+     2. attribute - attribute name
+     3. value - attribute value
+    """
     # obj.__dict__[attribute] = value [or]
     setattr(obj, attribute, value)
