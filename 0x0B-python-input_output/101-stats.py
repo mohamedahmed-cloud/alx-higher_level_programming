556696e320ffb396d724a7a511e5ab6895bc51f6
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-17 20:52:11 +0300

    add file 101-stats.py

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+while True:
+    status_dict = {200: 0, 301: 0, 400: 0, 401: 0,
+                   403: 0, 404: 0, 405: 0, 500: 0}
+    size = 0
+    for i in range(10):
+        all = input().split()
+        status = all[-2]
+        size += int(all[-1])
+        status_dict[int(status)] += 1
+
+    print(f"File size: {size}")
+    for key, value in status_dict.items():
+        if value:
+            print(f"{key}: {value}")
--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-append_after = __import__('100-append_after').append_after
+import random
+import sys
+from time import sleep
+import datetime
 
-append_after("txt.txt", "Python", "\"C is fun!\"\n")
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
