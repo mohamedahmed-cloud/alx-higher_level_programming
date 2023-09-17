7c0028722bab3ed7e7157249a972dbe3cbcaa82d
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-17 18:23:07 +0300

    add file 8-class_to_json.py

--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+MyClass = __import__('8-my_class').MyClass
+class_to_json = __import__('8-class_to_json').class_to_json
 
-filename = "my_list.json"
-my_list = [1, 2, 3]
-save_to_json_file(my_list, filename)
+m = MyClass("John")
+m.number = 89
+print(type(m))
+print(m)
 
-filename = "my_dict.json"
-my_dict = { 
-    'id': 12,
-    'name': "John",
-    'places': [ "San Francisco", "Tokyo" ],
-    'is_active': True,
-    'info': {
-        'age': 36,
-        'average': 3.14
-    }
-}
-save_to_json_file(my_dict, filename)
-
-try:
-    filename = "my_set.json"
-    my_set = { 132, 3 }
-    save_to_json_file(my_set, filename)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+mj = class_to_json(m)
+print(type(mj))
+print(mj)
