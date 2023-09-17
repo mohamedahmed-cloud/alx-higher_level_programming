5590a728208d5522bb399e658660532495c4c14c
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-17 15:41:38 +0300

    add file 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,9 +10,15 @@ from sys import argv
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
 file_name = "add_item.json"
+res = []
 
+if __name__ == "__main__":
+    try:
+        res = load_from_json_file(file_name)
+    except Exception:
+        pass
 
-save_to_json_file(argv[1:], file_name)
-
-load_from_json_file(file_name)
+    res.extend(argv[1:])
+    save_to_json_file(res, file_name)
