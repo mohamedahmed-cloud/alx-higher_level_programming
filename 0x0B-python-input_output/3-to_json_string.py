5586a967bd2a9fee8829fb656ac5543ee4ac55dc
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-17 13:40:52 +0300

    add file 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -8,6 +8,10 @@ to_json_string:  function that returns the JSON
 
 
 def to_json_string(my_obj):
+    """
+    args:
+        my_obj: the object need to make a seralize for it.
+    """
     json_obj = json.dumps(my_obj)
     # print(type(json_obj))
     return json_obj
