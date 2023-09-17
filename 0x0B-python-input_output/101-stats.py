6369cf4f17dfd58d204bfd08af1379b7fefac98c
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-17 21:28:43 +0300

    add file 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -20,18 +20,22 @@ Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
     status codes should be printed in ascending order
 """
 
+import sys
 
-while True:
-    status_dict = {200: 0, 301: 0, 400: 0, 401: 0,
-                   403: 0, 404: 0, 405: 0, 500: 0}
-    size = 0
-    for i in range(10):
-        all = input().split()
-        status = all[-2]
-        size += int(all[-1])
-        status_dict[int(status)] += 1
+try:
+    for line in sys.stdin:
+        status_dict = {200: 0, 301: 0, 400: 0, 401: 0,
+                       403: 0, 404: 0, 405: 0, 500: 0}
+        size = 0
+        for i in range(10):
+            all = input().split()
+            status = all[-2]
+            size += int(all[-1])
+            status_dict[int(status)] += 1
 
-    print(f"File size: {size}")
-    for key, value in status_dict.items():
-        if value:
-            print(f"{key}: {value}")
+        print(f"File size: {size}")
+        for key, value in status_dict.items():
+            if value:
+                print(f"{key}: {value}")
+except KeyboardInterrupt:
+    raise
