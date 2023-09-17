a35ab1726f3363a18f98e832c08293947234e351
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-17 18:57:30 +0300

    add file 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Student class
+"""
+
+
+class Student:
+    """
+    class have not any args
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        method : __init
+        args:
+            first_name
+            last_name
+            age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Return:
+            that retrieves a dictionary
+            representation of a Student instance
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
