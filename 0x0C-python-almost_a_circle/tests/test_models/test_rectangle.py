89cc671321cce74b8f552c81f00b883b096ca766
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-19 01:31:56 +0300

    upLoad file models/rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,14 +1,55 @@
-# import unittest
-# # import rectangle
-# from models.rectangle import Rectangle
+import unittest
+# import rectangle
+from models.rectangle import Rectangle
 
-# class TestRectangle(unittest.TestCase):
-#     def setUp(self) -> None:
-#         self.rectangle = Rectangle(1, 2)
+class TestRectangle(unittest.TestCase):
+    def test_getter(self):
+        # test initial condition
+        with self.assertRaises(ValueError):
+            Rectangle().width
+        with self.assertRaises(ValueError):
+            Rectangle().height
+        self.assertAlmostEqual(Rectangle(1, 1).x, 0)
+        self.assertAlmostEqual(Rectangle(1, 1).y, 0)
+        
+        # give negative value for width and height
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 1).width
+        with self.assertRaises(ValueError):
+            Rectangle(1, -1).height
+        self.assertAlmostEqual(Rectangle(1, 1).x, 0)
+        self.assertAlmostEqual(Rectangle(1, 1).y, 0)
 
-#     def test_getter(self):
-#         # 
-#         self.assertAlmostEqual(self.rectangle.width, 1)
-#         self.assertAlmostEqual(self.rectangle.height, 2)
-#         self.assertAlmostEqual(self.rectangle.x, 0)
-#         self.assertAlmostEqual(self.rectangle.y, 0)
+        # give 0 value for width and height
+        with self.assertRaises(ValueError):
+            Rectangle(0, 3).width
+        with self.assertRaises(ValueError):
+            Rectangle(3, 0).height
+        self.assertAlmostEqual(Rectangle(1, 1).x, 0)
+        self.assertAlmostEqual(Rectangle(1, 1).y, 0)
+
+        # give value foor all Rectangle __init__ parameters
+        # give x and y negative value
+        with self.assertRaises(ValueError):
+            Rectangle(1, 1, -1, 1).x
+        with self.assertRaises(ValueError):
+            Rectangle(1, 1, 1, -1).y
+        
+        # give value foor all Rectangle __init__ parameters
+        rectangle = Rectangle(1, 1, 1, 1)
+        self.assertAlmostEqual(rectangle.width, 1)
+        self.assertAlmostEqual(rectangle.height, 1)
+        self.assertAlmostEqual(rectangle.x, 1)
+        self.assertAlmostEqual(rectangle.y, 1)
+
+        # check on type
+        # type of width
+        with self.assertRaises(TypeError):
+            Rectangle("Yousef", 1, 1, 1).width
+        with self.assertRaises(TypeError):
+            Rectangle(1, "Yousef", 1, 1).height
+        with self.assertRaises(TypeError):
+            Rectangle(1, 1, "Yousef", 1).x
+        with self.assertRaises(TypeError):
+            Rectangle(1, 1, 1, "Yousef").y
+    
