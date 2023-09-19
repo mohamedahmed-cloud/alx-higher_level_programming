2504d68ac22251b3ab85402b1ec8dd81863b3c6e
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-09-19 16:44:46 +0300

    upLoad file models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,3 +32,21 @@ class Base:
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        save_to_file:  that writes the JSON string
+        representation of list_objs to a file:
+        """
+        name = list_objs[0].__class__.__name__
+        file_name = f"{name}.json"
+        with open(file_name, "w", encoding='utf-8') as f:
+            if list_objs is None:
+                f.write(cls.to_json_string('[]'))
+            else:
+                store = []
+                for obj in list_objs:
+                    store.append(obj.to_dictionary())
+                # print(store)
+                f.write(cls.to_json_string(store))
