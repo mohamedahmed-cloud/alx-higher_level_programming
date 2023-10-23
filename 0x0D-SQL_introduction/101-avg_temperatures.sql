9ab9cee9e79d0a32a509489c4e5e72aefe878210
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-24 02:22:42 +0300

    ADD ALIAS NAME TO COLUMN

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -2,4 +2,4 @@
 
 -- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 
-SELECT city, AVG(value) FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
