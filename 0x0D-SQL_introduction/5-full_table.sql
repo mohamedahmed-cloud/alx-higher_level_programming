a5252e3e47d89296348b754a8f961c7bb5fc1d09
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-24 00:52:01 +0300

    change column to create table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 -- We can't user describe or explain.
-select * FROM first_table;
+SHOW CREATE TABLE first_table;
