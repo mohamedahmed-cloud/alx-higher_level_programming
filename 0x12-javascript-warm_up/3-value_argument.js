8d3f647d2b612c83cb9b0b89ebbdfbfb0ae897c2
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-14 01:20:47 +0200

    add file 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 
+const argv = process.argv;
+let cnt = 0;
+
+argv.forEach(element => {
+  cnt++;
+});
+
+if (cnt === 2) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}
