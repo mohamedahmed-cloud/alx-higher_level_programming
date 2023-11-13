6386643c54ca73981047e8f30d84e8e4a3f8e777
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-14 01:57:25 +0200

    add file 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const argv = process.argv;
+const n1 = parseInt(argv[2]);
+
+if (isNaN(n1)) {
+  console.log(1);
+} else {
+  let ans = 1;
+  for (let i = 2; i <= n1; i++) {
+    ans *= i;
+  }
+  console.log(ans);
+}
