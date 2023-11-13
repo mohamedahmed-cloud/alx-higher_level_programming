8513de1e85891e8a142db3c010695cede318fae4
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-14 01:43:11 +0200

    add file 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 const argv = process.argv;
-const cnt = argv[2]
-if (cnt == 2) {
+const cnt = argv[2];
+
+if (argv.length === 2) {
   console.log('Missing number of occurrences');
 } else {
-  for(let i = 0; i < cnt; i++) {
+  for (let i = 0; i < cnt; i++) {
     console.log('C is fun');
-    }
+  }
 }
