0519ee13ab6373f999d56e13f65afc40c8e03e7a
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-14 01:17:29 +0200

    add file 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argv = process.argv
+const argv = process.argv;
 const length = argv.length;
-if (length == 2)
-	console.log('No argument');
-else if (length == 3)
-	console.log('Argument found');
-else
-	console.log('Arguments found');
-
+if (length === 2) {
+  console.log('No argument');
+} else if (length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
