357bf78bde669966076d3f301df567001fbf279c
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-14 01:33:51 +0200

    add file 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,8 +3,13 @@
 const argv = process.argv;
 const cnt = argv.length;
 
-if (cnt == 2) {
-  console.log("Not a number");
+if (cnt === 2) {
+  console.log('Not a number');
 } else {
-  console.log(parseInt(argv[3]));
+  const num = parseInt(argv[2]);
+  if (!isNaN(num)) {
+    console.log(`My number: ${num}`);
+  } else {
+    console.log('Not a number');
+  }
 }
