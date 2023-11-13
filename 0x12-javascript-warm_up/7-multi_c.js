8a84025a7c34ebe5eda6328a1d183d7eae1ae638
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-14 01:41:20 +0200

    add file 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const cnt = argv[2]
+if (cnt == 2) {
+  console.log('Missing number of occurrences');
+} else {
+  for(let i = 0; i < cnt; i++) {
+    console.log('C is fun');
+    }
+}
