fe6a0a1c333a75360db1c9a8322d0cffe95d530b
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-14 02:11:23 +0200

    add file 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const argv = process.argv;
+const n1 = parseInt(argv[2]);
+
+if (isNaN(n1)) {
+  console.log(0);
+} else {
+  let ans = Number.MIN_SAFE_INTEGER;
+  const newArgv = argv.slice(2).map(val => parseInt(val));
+  const mx = Math.max(...newArgv);
+
+  for (let i = 2; i < argv.length - 1; i++) {
+    if (argv[i] !== mx) {
+      ans = Math.max(argv[i], ans);
+    }
+  }
+  console.log(ans);
+}
