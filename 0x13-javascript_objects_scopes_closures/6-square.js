d944ab20b866572b65510c74c84bf406aed47e43
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-30 22:05:45 +0200

    add file 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const mainSquare = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends mainSquare {
   charPrint (chr = 'X') {
     super.print(chr);
   }
--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const nbOccurences = require('./7-occurrences').nbOccurences;
+const Square = require('./6-square');
 
-console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
-console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
+const s1 = new Square(4);
+s1.charPrint();
+
+s1.charPrint('C');
