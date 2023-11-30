817414ae6f1ad254f61a6b73e72405e329a86055
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-30 20:28:51 +0200

    add file 101-call_me_moby.js

--- 0x12-javascript-warm_up/push.sh
+++ 0x12-javascript-warm_up/push.sh
@@ -1,7 +1,7 @@
-# git add --chmod +x $1
-# semistandard ./$1
-# if [ $? -ne 1 ]; then
+git add --chmod +x $1
+semistandard ./$1
+if [ $? -ne 1 ]; then
 	git add *
 	git commit -m "add file $1"
 	git push
-# fi
+fi
