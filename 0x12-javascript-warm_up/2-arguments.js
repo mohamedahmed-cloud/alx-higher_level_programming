f9f894853568565db518d10de06606b77769da48
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-02-13 07:59:55 +0200

    add new file

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myVar = process.argv[2];
+if (process.argv[2] === undefined) {
+    console.log('No argument');
+}
+else if (process.argv[2] !== undefined && process.argv[3] === undefined) {
+    console.log('Argument found');
+}
+else if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
+    console.log('Arguments found');
+}
